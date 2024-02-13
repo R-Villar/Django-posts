179ceff1de8fd5a927605039ec6732a536ec28af
Remberto Villar <rembertovillar@gmail.com>
2024-02-13 13:38:14 -0500

    posts templates.

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.homepage, name="homepage"),
     path("about/", views.about, name="about"),
+    path("posts/", include("posts.urls")),
 ]
